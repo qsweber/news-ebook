1ce7933b608b58e5f0787b5995dbd077c38439ac
Quinn Weber <quinn@quinnweber.com>
2024-01-30 20:55:52 -0800

    new format

--- src/news_ebook/lib/news_source/economist.py
+++ src/news_ebook/lib/news_source/economist.py
@@ -64,51 +64,89 @@ def scrape_toc(date: str) -> typing.List[TocSection]:
     return [wtw, *sections]
 
 
-def _parse_and_scrape(article: TocArticle) -> Article:
-    print("Scraping {}".format(article.link))
-    article_soup = _scrape(article.link)
+"""
+{
+    'type': 'IMAGE',
+    'url': 'https://www.example.com/20240127_FBD001.jpg',
+    'altText': 'Description', (TODO implement scraping this)
+    'mode': 'NORMAL',
+    'imageType': 'ILLUSTRATION',
+    'caption': {'textHtml': ''},
+    'credit': 'Foo',
+    'source': None,
+    'width': 1280,
+    'height': 720,
+}
+"""
 
-    parsed = json.loads(
-        find_tag(
-            find_tag(article_soup, "head"), "script", {"type": "application/ld+json"}
-        ).text
-    )
 
-    print(parsed["image"])
+def get_image_paragraph(element: typing.Any) -> Paragraph:
+    img_data = requests.get(element["url"]).content
+    local_image_path = "output/images/{}".format(os.path.basename(element["url"]))
 
-    img_data = requests.get(parsed["image"]).content
-    local_image_path = "output/images/{}".format(os.path.basename(parsed["image"]))
     with open(local_image_path, "wb") as handler:
         handler.write(img_data)
 
-    text = parsed["articleBody"]
+    return Paragraph(
+        header=None,
+        image_path=local_image_path.strip("output/"),
+        text=None,  # Can I do alt-text?
+    )
 
-    paragraphs = [
-        Paragraph(
-            header=None,
-            image_path=None,
-            text=paragraph,
-        )
-        for paragraph in text.split("\n")
-    ]
+
+"""
+{
+    'type': 'PARAGRAPH',
+    'text': 'Foo bar',
+    'textHtml': '<p>Foo bar</p>'
+}
+"""
+
+
+def get_text_paragraph(element: typing.Any) -> Paragraph:
+    return Paragraph(header=None, image_path=None, text=element["text"])
+
+
+def get_paragraph(element: typing.Any) -> typing.Optional[Paragraph]:
+    if not element or not element["type"]:
+        print("can not find: {}", json.dumps(element))
+        return None
+
+    if element["type"] == "IMAGE":
+        return get_image_paragraph(element)
+    elif element["type"] == "PARAGRAPH":
+        return get_text_paragraph(element)
+
+    print("can not parse: {}", json.dumps(element))
+    return None
+
+
+def _parse_and_scrape(article: TocArticle) -> typing.Optional[Article]:
+    print("Scraping {}".format(article.link))
+    article_soup = _scrape(article.link)
+
+    parsed = json.loads(find_tag(article_soup, "script", {"id": "__NEXT_DATA__"}).text)
+    content = parsed["props"]["pageProps"]["cp2Content"]
+
+    if not content:
+        print("no cp2Content found")
+        return None
+
+    lead = content["leadComponent"]
+    body = content["body"]
+
+    elements = [lead, *body]
 
     return Article(
-        title=parsed["headline"],
-        description=parsed["description"],
-        paragraphs=[
-            Paragraph(
-                header=None,
-                image_path=local_image_path.strip("output/"),
-                text=None,
-            ),
-            *paragraphs,
-        ],
+        title=content["headline"],
+        description=content["rubric"],
+        paragraphs=[p for p in [get_paragraph(element) for element in elements] if p],
     )
 
 
 class Economist(NewsSource):
     def get_latest(self) -> Issue:
-        date = "2023-07-29"
+        date = "2024-01-27"
 
         sections = scrape_toc(date)
         issue = Issue(
@@ -117,14 +155,19 @@ class Economist(NewsSource):
                 Section(
                     title=section.title,
                     articles=[
-                        _parse_and_scrape(article)
-                        for article in section.articles
-                        if not article.link.startswith("/interactive")
+                        a
+                        for a in [
+                            _parse_and_scrape(article)
+                            for article in section.articles
+                            if not article.link.startswith("/interactive")
+                        ]
+                        if a
                     ],
                 )
                 for section in sections
                 if section.title != "Economic & financial indicators"
                 and section.title != "Graphic detail"
+                and section.title != "The world this week"
             ],
         )
 
